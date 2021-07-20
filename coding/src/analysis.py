2238046f7c63f852b709ff60b1ffe572b6f5b96f
Taylor Sorensen <taylorsorensen@Taylors-MacBook-Pro.local>
2021-07-20 16:29:43 -0600

    first complete draft

--- coding/src/analysis.py
+++ coding/src/analysis.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import os
 from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+from itertools import product
 
 class ExperimentResults():
     def __init__(self, results_path):
@@ -46,10 +49,14 @@ class ExperimentResults():
         '''
         Reads a list of files into a pandas dataframe. Supported filetypes: csv, pkl
         '''
+        print(file_names)
         dfs = []
         for file_name in file_names:
             dfs.append(self.read_df(file_name))
-        df = pd.concat(dfs, ignore_index=True)
+        if len(dfs) == 1:
+            df = dfs[0]
+        else:
+            df = pd.concat(dfs, ignore_index=True)
         return df
 
     def get_all_files(self, path, ends_with='.pkl'):
@@ -70,16 +77,19 @@ class ExperimentResults():
         '''
         files = self.get_all_files(self.results_path)
         self.results = self.read_and_combine_dfs(files)
-        # TODO - remove
+        # dropna
         self.results = self.results.dropna()
-        # self.results = self.results.sample(n=200)
+        # TODO - remove
+        # self.results = self.results.sample(n=1000)
+        # reset index
+        self.results = self.results.reset_index()
     
     def get_logprobs(self, response):
         '''
         Given an instance of gpt3 response, return the logprobs of the first sampled token.
         Returns a sorted list of tuples of (token, logprob)
         '''
-        logprobs = response['choices'][0].logprobs.top_logprobs[1]
+        logprobs = response['choices'][0].logprobs.top_logprobs[0]
         # sort by logprob
         logprobs = sorted(logprobs.items(), key=lambda x: x[1], reverse=True)
         return logprobs
@@ -105,12 +115,11 @@ class ExperimentResults():
         elif matching_strategy == 'substr':
             return token in category
     
-    def get_category_probs(self, response, categories, matching_strategy='substr'):
+    def get_category_probs(self, response, matching_strategy='substr'):
         '''
         Iterate through responses and return the relative probability for each category.
         Arguments:
             response (OpenAI GPT-3 response): gpt3 output results
-            categories (list): list of categories
             matching_strategy (str): matching strategy for logprobs to categories. Supported: 'exact', 'starts_with', 'substr'. Defaults to 'substr'.
                 'exact': exact match of token and category. Doesn't work for all categories since some are longer than 1 token.
                 'starts_with': category starts with token
@@ -121,10 +130,10 @@ class ExperimentResults():
         # get logprobs
         logprobs = self.get_logprobs(response)
         # get category probabilities
-        category_probs = {cat: 0 for cat in categories}
+        category_probs = {cat: 0 for cat in self.categories}
 
         for token, logprob in logprobs:
-            for category in categories:
+            for category in self.categories:
                 if self.matches(token, category, matching_strategy):
                     category_probs[category] += np.exp(logprob)
         
@@ -133,35 +142,36 @@ class ExperimentResults():
 
         return category_probs
     
-    def populate_category_probs(self):
+    def populate_category_probs(self, matching_strategy='substr', normalize_marginal=True):
         '''
         Populate category probs in self.results for each matching strategy. 
-        After running, self.results should contain columns 'probs_exact', 'probs_starts_with', and 'probs_substr'.
+        Arguments:
+            matching_strategy (str): matching strategy for logprobs to categories. Supported: 'exact', 'starts_with', 'substr'. Defaults to 'substr'.
+            normalize_marginal (bool): whether to normalize the marginal probability of each category. Defaults to True.
+        Afterwards, there should be a column for each category with total weight.
         '''
         # get all categories
-        categories = self.results['category'].unique()
+        self.categories = self.results['category'].unique().tolist()
         # populate category probs
-        for matching_strategy in self.matching_strategies:
-            column_name = 'probs_' + matching_strategy
-            self.results[column_name] = self.results.apply(lambda x: self.get_category_probs(x['response'], categories, matching_strategy), axis=1)
+        category_probs = self.results.apply(lambda x: self.get_category_probs(x['response'], matching_strategy), axis=1)
+        # to dataframe
+        category_probs = pd.DataFrame(category_probs.tolist())
+        # normalize by marginal probability
+        if normalize_marginal:
+            category_probs = category_probs.divide(category_probs.mean(axis=0), axis=1)
+        # normalize to sum to 1
+        category_probs = category_probs.divide(category_probs.sum(axis=1), axis=0)
+        # add to results
+        self.results = pd.concat([self.results, category_probs], axis=1)
     
-    # TODO - add a function to normalize category_probs by marginal probability of each category
-    
-    def argmax(self, category_probs):
-        '''
-        Given a dictionary of category_probs, return the category with the highest probability.
-        '''
-        return max(category_probs, key=category_probs.get)
-        
     def populate_scores(self):
         '''
         Populate score (0 if match, 1 otherwise) in self.results for each matching strategy.
         After running, self.results should contain columns 'score_exact', 'score_starts_with', 'score_substr'.
         '''
         # populate score
-        for matching_strategy in self.matching_strategies:
-            column_name = 'score_' + matching_strategy
-            self.results[column_name] = self.results.apply(lambda x: 1 if self.argmax(x['probs_' + matching_strategy]) == x['category'] else 0, axis=1)
+        self.results['guess'] = self.results[self.categories].idxmax(axis=1)
+        self.results['score'] = 1 * (self.results['guess'] == self.results['category'])
     
     def group_by_columns(self, columns, df=None):
         '''
@@ -172,42 +182,100 @@ class ExperimentResults():
         '''
         if df is None:
             df = self.results
-        # df = df.groupby(columns).agg({'score_exact': 'mean', 'score_starts_with': 'mean', 'score_substr': 'mean'})
-        df = df.groupby(columns).agg({'score_starts_with': 'mean', 'score_substr': 'mean'})
-        # drop all columns except [*columns, 'score_exact', 'score_starts_with', 'score_substr']
-        # keep_columns = columns + ['score_exact', 'score_starts_with', 'score_substr']
-        # keep_columns = columns + ['score_starts_with', 'score_substr']
-        # df = df[keep_columns]
-        # # set index to columns
-        # df = df.set_index(columns)
+        df = df.groupby(columns).agg({'score': 'mean'})
         return df
     
-    def plot_n(self, split_by=[]):
+    def plot_n(self, split_by=[], average_with_error_bars=False):
         '''
         Plots the n column, and splits runs by the colums in split_by.
         Arguments:
             split_by (list): list of columns to split by
+            average_with_error_bars (bool): whether to plot average and error bars. Defaults to False.
         '''
         # check if split_by is a list, if not, make it a list
         if not isinstance(split_by, list):
             split_by = [split_by]
-        df = self.group_by_columns(['n'] + split_by)
+        df = self.group_by_columns(split_by + ['n'])
+
         for col in df.columns:
             df_col = df[col]
             if len(split_by) > 0:
-                # get all unique index values for level 1
-                index_values = df_col.index.get_level_values(1).unique()
-                # plot each run for n
-                for ix in index_values:
-                    plt.plot(df_col.loc[:, ix])
+                if average_with_error_bars:
+                    means = df_col.groupby('n').agg('mean')
+                    std = df_col.groupby('n').agg('std')
+                    # plot with shaded error bars
+                    plt.plot(means.index, means, label=col)
+                    plt.fill_between(means.index, means - 2*std, means + 2*std, alpha=0.2)
+                else:
+                    n_levels = len(split_by)
+                    runs = list(product(*[df_col.index.get_level_values(i).unique() for i in range(n_levels)]))
+                    for run in runs:
+                        if n_levels == 1:
+                            run = df_col.loc[run[0]]
+                        elif n_levels == 2:
+                            run = df_col.loc[run[0], run[1]]
+                        elif n_levels == 3:
+                            run = df_col.loc[run[0], run[1], run[2]]
+                        else:
+                            raise ValueError('split_by must be of length <= 3')
+                        plt.plot(run.index, run, alpha=.5)
             else:
-                plt.plot(df)
+                plt.plot(df_col)
             plt.title(col)
+            plt.xlabel('n')
             plt.show()
         pass
+    
+    def plot_confusion_matrix(self):
+        '''
+        Plot a confusion matrix for all categories. Guesses located in 'guess' and target in 'categories' in self.results.
+        '''
+        # TODO - sort categories in an intelligent way
+        # get categories
+        categories = self.categories
+        # get guesses
+        guesses = self.results['guess'].unique().tolist()
+        # get confusion matrix
+        cm = confusion_matrix(self.results['category'], self.results['guess'])
+        # plot
+        # make big figure
+        fig, ax = plt.subplots(figsize=(10, 10))
+        # title
+        plt.title('Confusion Matrix')
+        plt.imshow(cm, cmap='Blues', interpolation='nearest')
+        plt.colorbar()
+        tick_marks = np.arange(len(categories))
+        plt.xticks(tick_marks, categories, rotation=90)
+        plt.yticks(tick_marks, guesses)
+        plt.ylabel('Guess')
+        plt.xlabel('Target')
+        plt.show()
+    
+    def plot_category_accuracies(self):
+        '''
+        Plot a bar chart of accuracies, in order of accuracy.
+        '''
+        # get accuracies
+        accuracies = self.results.groupby('category').agg({'score': 'mean'})
+        # sort accuracies
+        accuracies = accuracies.sort_values(by='score', ascending=False)
+        # plot
+        sns.barplot(accuracies.index, accuracies['score'])
+        plt.ylim(0,1)
+        plt.title('Accuracies')
+        plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+        plt.subplots_adjust(bottom=0.5)
+        plt.show()
+
+
 
 if __name__ == '__main__':
     er = ExperimentResults('experiments/nyt/07-16-2021/examples_instances_runs/combined/')
+    # er = ExperimentResults('experiments/nyt/07-06-21/slash/')
     # df = er.group_by_columns(['n'])
-    er.plot_n('exemplar')
+    er.plot_category_accuracies()
+    er.plot_confusion_matrix()
+    er.plot_n(['exemplar', 'instance'], average_with_error_bars=True)
+    er.plot_n(['exemplar', 'instance'], average_with_error_bars=False)
+    breakpoint()
     pass
