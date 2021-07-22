bef591299203a4cba4752f57c250251d8d799923
Chris Rytting <christophermichaelrytting@gmail.com>
2021-07-22 08:46:45 -0600

    Added directory for tests and ran last little bit of EI experiment

--- coding/experiments/nyt/07-20-2021/EI/gpt3.py
+++ coding/experiments/nyt/07-20-2021/EI/gpt3.py
@@ -14,7 +14,7 @@ dataset = 'nyt'
 exp_name = 'EI'
 today = date.today()
 yesterday = today - timedelta(days=1)
-exp_dir = f'experiments/{dataset}/{yesterday.strftime("%m-%d-%Y")}/{exp_name}/fill'
+exp_dir = f"experiments/{dataset}/{yesterday.strftime('%m-%d-%Y')}/{exp_name}"
 pccfs_dir = '/mnt/pccfs2/backed_up/crytting/partisanbrain/coding'
 exp_dir = os.path.join(pccfs_dir, exp_dir)
 
@@ -23,38 +23,37 @@ if os.path.isdir(exp_dir):
 else:
     os.makedirs(exp_dir)
 
-# templatizer = Templatizer(dataset_name='nytimes')
-# output = templatizer.templatize_many(
-#     ns_per_category=[1, 2, 3, 4],
-#     ns_exemplars=[1, 2, 3, 4, 5],
-#     n_exemplar_runs=5,
-#     n_instance_runs=5,
-# )
-# output = templatizer.templatize_many()
+templatizer = Templatizer(dataset_name='nytimes')
+output = templatizer.templatize_many(
+    ns_per_category=[1],
+    ns_exemplars=[0],
+    n_exemplar_runs=1,
+    n_instance_runs=1,
+)
+    # ns_per_category=[1, 2, 3, 4],
+    # n_exemplar_runs=5,
+    # n_instance_runs=5,
 
-for pkl in 'eps1.pickle eps2.pickle'.split():
-    output = rpkl(os.path.join(exp_dir, pkl))
+responses = []
 
-    responses = []
+print('Generating Completions')
+for i, row in tqdm(output.iterrows()):
+    response = None
+    try:
+        print(i, row)
+        breakpoint()
+        # response = openai.Completion.create(engine="davinci", prompt=row.prompt, max_tokens=1, logprobs=100)
+    except Exception as exc:
+        print(exc)
+        pass
+    responses.append(response)
+    # if i % 1000 == 0 or i == len(output) - 1:
+    #     pklpath = os.path.join(exp_dir,f'responses.pickle')
+    #     with open(pklpath, 'wb') as f:
+    #         pickle.dump(responses, f)
 
-    print(f'Generating Completions for {pkl}')
-    for i, row in tqdm(output.iterrows()):
-        response = None
-        try:
-            print(i, row)
-            breakpoint()
-            # response = openai.Completion.create(engine="davinci", prompt=row.prompt, max_tokens=1, logprobs=100)
-        except Exception as exc:
-            print(exc)
-            pass
-        responses.append(response)
-        if i % 1000 == 0 or i == len(output) - 1:
-            pklpath = os.path.join(exp_dir,f'{pkl}responses.pickle')
-            with open(pklpath, 'wb') as f:
-                pickle.dump(responses, f)
-
-    # Write output df to pickle
-    output['responses'] = responses
-    pklpath = os.path.join(exp_dir,'EI.pickle')
-    with open(pklpath, 'wb') as f:
-        pickle.dump(output, f)
+# Write output df to pickle
+output['responses'] = responses
+pklpath = os.path.join(exp_dir,'EI.pickle')
+with open(pklpath, 'wb') as f:
+    pickle.dump(output, f)
