8c766b3e70b2b23c5e6aa4885bf1dd9b727e7b44
Josh Robinson <Robinson.josh.j@gmail.com>
2021-08-23 16:44:32 -0600

    Factor out _make_prompt code to dataset.py

--- survey/src/dataset.py
+++ survey/src/dataset.py
@@ -21,6 +21,13 @@ class Dataset(abc.ABC):
         self._exemplars = self._data.loc[self._data.index.isin(exemplar_idxs)]
         self._data = self._data.loc[~self._data.index.isin(exemplar_idxs)]
 
+        # Get row and exemplar backstories (so they only need
+        # to be calculated once)
+        self._row_backstories = {idx: self._make_backstory(row) for (idx, row)
+                                 in self._data.iterrows()}
+        self._exemplar_backstories = [self._make_backstory(row) for (_, row)
+                                      in self._exemplars.iterrows()]
+
     @property
     def data(self):
         return self._data
@@ -28,6 +35,14 @@ class Dataset(abc.ABC):
     @property
     def exemplars(self):
         return self._exemplars
+
+    @property
+    def row_backstories(self):
+        return self._row_backstories
+
+    @property
+    def exemplar_backstories(self):
+        return self._exemplar_backstories
 
     @abc.abstractmethod
     def _format(self, df):
@@ -52,8 +67,23 @@ class Dataset(abc.ABC):
         """
         pass
 
+    def _make_prompt(self, row_idx, col_name, func):
+        row_backstory = self._row_backstories[row_idx]
+
+        exemplar_dv_strs = [func(e[col_name]) for (_, e)
+                            in self._exemplars.iterrows()]
+        blank_dv_str = func(None)
+        prompt = ""
+        for i in range(len(self._exemplars)):
+            prompt += self._exemplar_backstories[i] + " "
+            prompt += exemplar_dv_strs[i] + "\n"
+        prompt += row_backstory + " "
+        prompt += blank_dv_str
+
+        return prompt
+
     @abc.abstractmethod
-    def _make_prompts(self, row, exemplars=None):
+    def _make_prompts(self, row_idx):
         """
         Here subclass should implement converting a pandas row
         and exemplars dataframe to a list of prompt strings. This
--- survey/src/survey_datasets.py
+++ survey/src/survey_datasets.py
@@ -28,13 +28,15 @@ class ExampleSurveyDataset(Dataset):
         return new_df
 
     def _get_shot_first_str(self, value):
-        if len(value) != 0:
-            value = " " + value
-        return ("Between Han and Greedo I think the one "
-                f"who shot first was{value}")
+        if value is None:
+            return ("Between Han and Greedo I think the one "
+                    "who shot first was")
+        else:
+            return ("Between Han and Greedo I think the one "
+                    f"who shot first was {value}")
 
     def _get_fan_str(self, value):
-        if value == "":
+        if value is None:
             return "When asked if I'm a Star Wars fan I say"
         else:
             return ("When asked if I'm a Star Wars fan "
@@ -43,40 +45,21 @@ class ExampleSurveyDataset(Dataset):
     def _make_backstory(self, row):
         return f"I am a {row['age']} year old {row['gender'].lower()}."
 
-    def _make_prompts(self, row, exemplars):
+    def _make_prompts(self, row_idx):
+
         prompts = []
-        row_backstory = self._make_backstory(row)
-        exemplar_backstories = [self._make_backstory(e) for (_, e) in exemplars.iterrows()]
 
-        # Our first prompt is for the shot_first DV
-        exemplar_shot_firsts = [self._get_shot_first_str(e["shot_first"]) for (_, e)
-                                in exemplars.iterrows()]
-        shot_first_blank = self._get_shot_first_str("")
-        prompt = ""
-        for i in range(len(exemplars)):
-            prompt += exemplar_backstories[i] + " "
-            prompt += exemplar_shot_firsts[i] + "\n"
-        prompt += row_backstory + " "
-        prompt += shot_first_blank
-
-        prompts.append(prompt)
+        # Our first prompt is for the shot first DV
+        p = self._make_prompt(row_idx, "shot_first", self._get_shot_first_str)
+        prompts.append(p)
 
         # Our second prompt is for the fan DV
-        exemplar_fans = [self._get_fan_str(e["fan"]) for (_, e)
-                         in exemplars.iterrows()]
-        fan_blank = self._get_fan_str("")
-        prompt = ""
-        for i in range(len(exemplars)):
-            prompt += exemplar_backstories[i] + " "
-            prompt += exemplar_fans[i] + "\n"
-        prompt += row_backstory + " "
-        prompt += fan_blank
-
-        prompts.append(prompt)
+        p = self._make_prompt(row_idx, "fan", self._get_fan_str)
+        prompts.append(p)
 
         return prompts
 
 
 if __name__ == '__main__':
     ds = ExampleSurveyDataset(n_exemplars=5)
-    print(ds._make_prompts(ds.data.iloc[0], ds.exemplars))
+    print(ds._make_prompts(400))
