9e068e05c5ab4ec7a63fbfb45c73653874c69017
Josh Robinson <Robinson.josh.j@gmail.com>
2021-08-23 12:18:15 -0600

    Move example dataset code

--- survey/src/dataset.py
+++ survey/src/dataset.py
@@ -50,20 +50,3 @@ class Dataset(abc.ABC):
 
     def _get_exemplar_idxs(self, n):
         return random.sample(range(len(self._data)), n)
-
-
-class PewDataset(Dataset):
-
-    def __init__(self, n_exemplars):
-        pew_fname = "../data/Pew Research Center Spring 2016 Global Attitudes Dataset WEB FINAL.sav"
-        super().__init__(pew_fname, n_exemplars)
-
-    def _make_prompt(self, row):
-        return "This is a prompt"
-
-    def _format(self, df):
-        return df[df["country"] == "United States"]
-
-
-if __name__ == '__main__':
-    ds = PewDataset(n_exemplars=5)
---
+++ survey/src/survey_datasets.py
@@ -0,0 +1,19 @@
+from dataset import Dataset
+
+
+# This is an example that will be removed
+class PewDataset(Dataset):
+
+    def __init__(self, n_exemplars):
+        pew_fname = "../data/Pew Research Center Spring 2016 Global Attitudes Dataset WEB FINAL.sav"
+        super().__init__(pew_fname, n_exemplars)
+
+    def _make_prompt(self, row):
+        return "This is a prompt"
+
+    def _format(self, df):
+        return df[df["country"] == "United States"]
+
+
+if __name__ == '__main__':
+    ds = PewDataset(n_exemplars=5)
