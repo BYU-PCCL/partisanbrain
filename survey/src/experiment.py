693fdae548ec42c2161cd6b6b6d85675c8a7b95d
Josh Robinson <Robinson.josh.j@gmail.com>
2021-08-23 12:09:23 -0600

    Make some outlining commennts

--- survey/src/experiment.py
+++ survey/src/experiment.py
@@ -6,8 +6,13 @@ class Experiment:
 
     def run(self):
         """Get results from GPT-3 API"""
+        # Iterate over the rows of dataset
+        #   For each row make prompt
+        #   Pass prompt to GPT-3
+        #   Store result in results
         pass
 
     def store_results(self):
         """Store results obtained from run method"""
+        # Save results
         pass
