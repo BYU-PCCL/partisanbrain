f3e914f5238b480befc2a7a228c4aeba21746282
Josh Robinson <Robinson.josh.j@gmail.com>
2021-08-23 18:27:31 -0600

    Fix pickle loading

--- survey/src/result_explorer.py
+++ survey/src/result_explorer.py
@@ -5,4 +5,10 @@ class ResultExplorer:
     """For getting statistics and visualizing results of an experiment"""
 
     def __init__(self, result_fname):
-        self._results = pickle.load(result_fname)
+        with open(result_fname, "rb") as f:
+            self._results = pickle.load(f)
+        print(self._results)
+
+
+if __name__ == "__main__":
+    re = ResultExplorer("star_wars_results.pkl")
