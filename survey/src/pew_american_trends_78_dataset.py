eb7f4c158e7a7a53d30ec884e36fa70e09dd03df
Josh Robinson <Robinson.josh.j@gmail.com>
2021-08-25 14:17:20 -0600

    Improve filename

--- survey/src/pew_american_trends_78_dataset.py
+++ survey/src/pew_american_trends_78_dataset.py
@@ -1,49 +1,4 @@
-####################################################################################
-# Josh
-####################################################################################
-# class PewAmericanTrendsDataset(Dataset):
-
-#     def __init__(self, n_exemplars):
-#         survey_fname = "data/ATP W78.sav"
-#         super().__init__(survey_fname, n_exemplars)
-
-#     def _format(self, df):
-
-#         demographic_col_names = ["F_AGECAT",
-#                                  "F_GENDER",
-#                                  "F_PARTY_FINAL",
-#                                  "F_EDUCCAT",
-#                                  "F_IDEO",
-#                                  "F_INCOME",
-#                                  "F_RELIG",
-#                                  RACE,
-#                                  REGION,
-#                                  "F_MARITAL"]
-
-#         dv_col_names = ["ECON1_W78",
-#                         "ECON1B_W78",
-#                         "SATIS_W78",
-#                         "VTADMIN_POST_US_W78",
-#                         "ELECTRESULTPLAT_W78",
-#                         "COVID_2ASSISTLD_W78",
-#                         "POL12_W78",
-#                         "COVID_OPENMORE_W78",
-#                         "DIVISIONSCONC_W78",
-#                         "VOTELIST_US_W78",
-#                         ]
-
-#         new_df = df[demographic_col_names]
-
-#         return df
-
-#     def _make_backstory(self, row):
-#         age_str = row["F_AGECAT"]  # ['18-29', '30-49', '50-64', '65+', 'Refused']
-#         backstory = f"I am between {} and {} years old."
-
-#         return "Backstory"
-
-#     def _get_prompt_instructions(self):
-#         return {}    
+from dataset import Dataset
 
 
 class PewAmericanTrendsWave78Dataset(Dataset):
@@ -114,13 +69,14 @@ class PewAmericanTrendsWave78Dataset(Dataset):
         new_df = new_df[new_df["party"].isin(["Democrat",
                                               "Republican",
                                               "Independent"])]
+        new_df = new_df[new_df["religion"] != "Other"]
 
         for col_name in list(new_df):
             new_df = new_df[new_df[col_name] != "Refused"]
 
         # Randomly sample 500 + self._n_exemplars rows
         new_df = new_df.sample(n=500+self._n_exemplars, random_state=0)
-        print(new_df["party"].unique())
+        print(new_df["religion"].unique().tolist())
 
         return new_df
 
@@ -146,25 +102,43 @@ class PewAmericanTrendsWave78Dataset(Dataset):
         else:
             backstory.append(f"In terms of political parties I am {row['party']}")
 
-        # 
+        # TODO: Education
+
+        # TODO: Ideology
+        if row["ideo"] in ["Conservative", "Very conservative"]:
+            ideology = "conservative"
+        elif row["ideo"] in ["Liberal", "Very liberal"]:
+            ideology = "liberal"
+        else:
+            ideology = "neutral"
+
+        backstory.append(("In terms of political ideology, "
+                          f"I'd consider myself to be {ideology}."))
+
+
+        # TODO: Income
+
+        # TODO: Religiosity
+        if row["religion"] == "Nothing in particular":
+            backstory.append("I don't identify with any religion in particular.")
+        else:
+            if "Orthodox" in row["religion"]:
+                religion == "Orthodox"
+            elif "Mormon" in row["religion"]:
+                religion = "Mormon"
+            elif row["religion"] in ["Atheist", "Agnostic"]:
+                religion = row["religion"].lower()
+            else:
+                religion = row["religion"]
+            print(f"In terms of religion I am {religion}")
+
+        # TODO: Race/Ethnicity
+
+        # TODO: Region
+
+        # TODO: Marital Status
 
         return backstory
-
-    def _get_prompt_instructions(self):
-        return {}
-
-
-class PewAmericanTrendsWave67Dataset(Dataset):
-
-    def __init__(self, n_exemplars):
-        survey_fname = "data/StarWars.csv"
-        super().__init__(survey_fname, n_exemplars)
-
-    def _format(self, df):
-        return df
-
-    def _make_backstory(self, row):
-        return "Backstory"
 
     def _get_prompt_instructions(self):
         return {}
