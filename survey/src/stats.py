cd8cd6724261db680a4d4c080e86a0e56635154d
Josh Robinson <Robinson.josh.j@gmail.com>
2021-09-04 12:59:43 -0600

    Commit work in progress

--- survey/src/stats.py
+++ survey/src/stats.py
@@ -1,12 +1,31 @@
 from scipy.stats import chi2_contingency
-from sklearn.metrics import confusion_matrix
 
 import numpy as np
 
 
+def list_to_val_map(x):
+    val_map = dict()
+    n = 0
+    for val in x:
+        if val not in val_map.keys():
+            val_map[val] = n
+            n += 1
+    return val_map
+
+
+def get_pairwise_counts(a, b):
+    a_map = list_to_val_map(a)
+    b_map = list_to_val_map(b)
+    tbl = np.zeros((len(a_map), len(b_map)))
+    pairs = zip(a, b)
+    for pair in pairs:
+        tbl[a_map[pair[0]], b_map[pair[1]]] += 1
+    return tbl
+
+
 def cramers_v_from_vecs(a, b):
     """cramers_v function but including making confusion matrix"""
-    return cramers_v(confusion_matrix(a, b))
+    return cramers_v(get_pairwise_counts(a, b))
 
 
 def cramers_v(cm):
