d020bf6efa53ad8e811b9e0bc8e3345790a9c70f
Taylor Sorensen <taylorsorensen@Taylors-MacBook-Pro.local>
2021-10-08 16:25:41 -0600

    added baseclass

--- mutualinf/infra/lmsampler_baseclass.py
+++ mutualinf/infra/lmsampler_baseclass.py
@@ -1,13 +1,8 @@
 from abc import ABCMeta, abstractmethod
 
-class LMSampler(metaclass=ABCMeta):
+class LMSamplerBaseClass(metaclass=ABCMeta):
     def __init__(self, model_name):
         self.model_name = model_name
-
-    @property
-    @abstractmethod
-    def model_name(self):
-        pass
 
     @abstractmethod
     def send_prompt(self, prompt, n_probs):
@@ -17,8 +12,6 @@ class LMSampler(metaclass=ABCMeta):
             prompt (str) a prompt to be sent to LM
             n_probs (int) number of desired output probalities.
         Return:
-            dict (str:int) a dictionary of probabilities of length n_probs
+            dict (str:int) a dictionary of log probabilities of length n_probs
         '''
         pass
-
-    
