29104c9e477dbd49f0adde94685f88ffce942ec5
Mahmoud-I-Khalil <mkhalilbyu@gmail.com>
2021-10-19 19:34:54 -0600

    copa.py with 20 templates, tested in playground

--- mutualinf/copa.py
+++ mutualinf/copa.py
@@ -45,11 +45,24 @@ SHOTS = [
 
 class CopaDataset(Dataset):
     def __init__(self, sample_seed=0, n=None):
-        self.token_set_dict = None
+        self.token_set_dict = {}
         super().__init__(sample_seed=sample_seed, n=n)
 
     def _modify_raw_data(self, df):
         return df
+
+    # Wanted to create something that gives you why
+    # def _because_so_maker(self,word):
+    #     if(word == "cause"):
+    #         return "because"
+    #     else:
+    #         return "so"
+    
+    # def _what_why_maker(self,word):
+    #     if(word == "cause"):
+    #         return "What caused"
+    #     else:
+    #         return "Why"
 
     def _get_templates(self):
 
@@ -114,8 +127,8 @@ class CopaDataset(Dataset):
                                 f"Premise: {row['premise']}\n"
                                 f"Choice 1. {row['choice1']}\n"
                                 f"Choice 2. {row['choice2']}\nAnswer: Choice"), self.token_set_dict),
-#-----------------------------------------UNTESTED YET--------------------------------------------------------
-            'what_is_cause/effect_of_premise' : (
+
+            'what_is_cause_effect_of_premise' : (
 
                 lambda row : (  f'What is the {row.question} of the following premise:"{row.premise}"\n\n'
                                 f'Choice 1. {row.choice1}\n'
@@ -123,7 +136,7 @@ class CopaDataset(Dataset):
                                 f'Answer: Choice'), self.token_set_dict
                             ),
 
-            'what_is_cause/effect_of_premise_given_smthn' : (
+            'what_is_cause_effect_of_premise_given_smthn' : (
 
                 lambda row : (  f'Choose between Choice 1 or Choice 2\n'
                                 f'What is the {row.question} of the following premise:"{row.premise}"\n\n'
@@ -132,7 +145,7 @@ class CopaDataset(Dataset):
                                 f'Answer: Choice'), self.token_set_dict
                             ),
                             
-            'what_is_cause/effect_of_premise_given1or2' : (
+            'what_is_cause_effect_of_premise_given1or2' : (
 
                 lambda row : (  f'What is the {row.question} of the following premise:"{row.premise}"\n\n'
                                 f'If asked to choose between '
@@ -140,6 +153,7 @@ class CopaDataset(Dataset):
                                 f'Choice 2: "{row.choice2}"\n'
                                 f'My answer would be: Choice'), self.token_set_dict
                             ),
+
 
             'Based_on_this_premise_1' : (
 
@@ -155,54 +169,58 @@ class CopaDataset(Dataset):
 
                 lambda row : (  f'Based on this premise:"{row.premise}"\n\n'
                                 f'If asked to pick between\n'
-                                f'Choice 1:"{row.choice1}"'
+                                f'Choice 1:"{row.choice1}" '
                                 f'or'
-                                f'Choice 2: "{row.choice2}" to get the{row.question}\n'
+                                f'Choice 2: "{row.choice2}" to get the {row.question}\n'
                                 f'I would say:"Choice'), self.token_set_dict
                             ),
-            
-            'Introducing_questions_then_premise_1' : (
 
-                lambda row : (  f'If asked to pick between '
-                                f'choice 1:"{row.choice1}" or '
-                                f'choice 2:"{row.choice2}" to see what {row.question}d the premise:"{row.premise}"\n'
-                                f'I would say:"choice'), self.token_set_dict
-                            ),
-
-            'Introducing_questions_then_premise_2' : (
-
-                lambda row : (  f'If asked to choose between '
-                                f'1:"{row.choice1}" or '
-                                f'2:"{row.choice2}" to see what {row.question}d the premise:"{row.premise}"\n'
-                                f'I would say:"'), self.token_set_dict
-                            ),
-            
             'I_want_to_figure_out' : (
 
-                lambda row : (  f'I want to figure out what {row.question}d the premise:"{row.premise}"\n'
+                lambda row : (  f'I want to figure out what is the {row.question} of:"{row.premise}"\n'
                                 f'Choice 1:"{row.choice1}" or '
                                 f'choice 2:"{row.choice2}"\n'
                                 f'I would say:"choice'), self.token_set_dict
                             ),
-            
-            'NoChoices' : (
 
-                lambda row : (  f'What {row.question}d the Premise:"{row.premise}"\n'
-                                f'1:"{row.choice1}"\n'
-                                f'2:"{row.choice2}"\n'
-                                f'My answer would be:"'), self.token_set_dict
-                            ),
-            
             'DirectChoice' : (
 
-                lambda row : (  f'What {row.question}d the Premise:"{row.premise}"\n'
+                lambda row : (  f'What {row.question}ed the Premise:"{row.premise}"\n'
                                 f'Choose between "1" or "2"\n'
                                 f'1:"{row.choice1}"\n'
                                 f'2:"{row.choice2}"\n'
                                 f'Answer:"'), self.token_set_dict
+                            ),   
+
+             'Introducing_questions_then_premise_1' : (
+
+                lambda row : (  f'If asked to pick between '
+                                f'choice 1:"{row.choice1}" or '
+                                f'choice 2:"{row.choice2}" to see what was the {row.question} of this premise:"{row.premise}"\n'
+                                f'I would say:"choice'), self.token_set_dict
                             ),
-            
+
+             'my_fav_approach' : (
+
+                lambda row : (  f'Read the following premise and answer by choosing "{row.question}1" or "{row.question}2"\n'
+                                f'Premise:"{row.premise}"\n'
+                                f'{row.question}1:"{row.choice1}"\n'
+                                f'{row.question}2:"{row.choice2}"\n'
+                                f'Answer:"{row.question}'), self.token_set_dict
+                            ),
+
+             'my_fav_approach_flipped_order' : (
+
+                lambda row : (  f'Read the following premise and pick "{row.question}2" or "{row.question}1"\n'
+                                f'Premise:"{row.premise}"\n'
+                                f'{row.question}1:"{row.choice1}"\n'
+                                f'{row.question}2:"{row.choice2}"\n'
+                                f'Answer:"{row.question}'), self.token_set_dict
+                            ),
+
         }
+
+        return templates
 
 
 if __name__ == '__main__':
