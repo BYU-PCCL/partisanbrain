cae9d0549f7035f23c811f1c5471766d4b868583
Taylor Sorensen <taylorsorensen@Taylors-MacBook-Pro.local>
2021-10-25 12:29:31 -0600

    added prompts

--- mutualinf/common_sense_qa.py
+++ mutualinf/common_sense_qa.py
@@ -4,6 +4,28 @@ from infra_modules import Dataset
 import json
 import pandas as pd
 
+SHOTS = [
+'''Question: I'm crossing the river, my feet are wet but my body is dry, where am I?
+Choices: bridge, waterfall, valley, pebble, mountain
+Answer: "valley" is the best answer. While "bridge" also seems to make sense at first, your feet would not be wet if you crossed over a river on a bridge. Meanwhile, if you crossed the river at a valley, the river would be shallow, only getting your feet wet.''',
+
+'''Question: In what Spanish speaking North American country can you get a great cup of coffee?
+Choices: mildred's coffee shop, mexico, diner, kitchen, canteen
+Answer: "mexico" is the best answer. It's true that you can get a cup of coffee in a coffee shop or a diner, but the question specifically asks for a Spanish speaking North American country. Mexico is the only country listed, so that must be the correct answer.''',
+
+'''Question: I'm crossing the river, my feet are wet but my body is dry, where am I?
+Choices: bridge, waterfall, valley, pebble, mountain
+Answers (in order of best to worst): valley, bridge, waterfall, mountain, pebble''',
+
+'''Question: In what Spanish speaking North American country can you get a great cup of coffee?
+Choices: mildred's coffee shop, mexico, diner, kitchen, canteen
+Answers (in order of best to worst): mexico, mildred's coffee shop, diner, kitchen, canteen''',
+
+'''"I'm crossing the river, my feet are wet but my body is dry, where am I?", "bridge, waterfall, valley, pebble, mountain", -> "valley"''',
+
+'''"In what Spanish speaking North American country can you get a great cup of coffee?", "mexico, mildred's coffee shop, diner, kitchen, canteen", -> "mexico"''',
+]
+
 
 class CommonSenseQaDataset(Dataset):
 
@@ -14,6 +36,8 @@ class CommonSenseQaDataset(Dataset):
         self._token_set = lambda row: {
             k: row[k].split(" ")[0] for k in ["A", "B", "C", "D", "E"]
         }
+
+        self.alphabet_token_set = ["A", "B", "C", "D", "E"]
 
         super().__init__(sample_seed=sample_seed,
                          n=n,
@@ -125,6 +149,55 @@ class CommonSenseQaDataset(Dataset):
                                   f"\"{row['E']}.\"\n\n"
                                   "Student: I know the right answer - it's \""),
                      self._token_set),
+
+            
+            'explanation_1shot': (lambda row: (
+                f"Choose the best single answer to the question, and explain your answer.\n\n" +
+                SHOTS[0] + '\n\n' +
+                f"Question: {row['question']}\n"
+                f"Choices: {row['A']}, {row['B']}, {row['C']}, {row['D']}, {row['E']}\n"
+                f"Answer: \""), self._token_set),
+
+            'explanation_2shot': (lambda row: (
+                f"Choose the best single answer to the question, and explain your answer.\n\n" +
+                SHOTS[0] + '\n\n' +
+                SHOTS[1] + '\n\n' +
+                f"Question: {row['question']}\n"
+                f"Choices: {row['A']}, {row['B']}, {row['C']}, {row['D']}, {row['E']}\n"
+                f"Answer: \""), self._token_set),
+            
+            'best_worst_0shot': (lambda row: (
+                f"Given the question, order the options from best answer to the question to worst answer to the question.\n\n"
+                f"Question: {row['question']}\n"
+                f"Choices: {row['A']}, {row['B']}, {row['C']}, {row['D']}, {row['E']}\n"
+                f"Answers (in order of best to worst):"), self._token_set),
+
+            'best_worst_1shot': (lambda row: (
+                f"Given the question, order the options from best answer to the question to worst answer to the question.\n\n" + 
+                SHOTS[2] + '\n\n' +
+                f"Question: {row['question']}\n"
+                f"Choices: {row['A']}, {row['B']}, {row['C']}, {row['D']}, {row['E']}\n"
+                f"Answers (in order of best to worst):"), self._token_set),
+
+            'best_worst_2shot': (lambda row: (
+                f"Given the question, order the options from best answer to the question to worst answer to the question.\n\n" + 
+                SHOTS[2] + '\n\n' +
+                SHOTS[3] + '\n\n' +
+                f"Question: {row['question']}\n"
+                f"Choices: {row['A']}, {row['B']}, {row['C']}, {row['D']}, {row['E']}\n"
+                f"Answers (in order of best to worst):"), self._token_set),
+            
+            'open_ai_1shot': (lambda row: (
+                f"Given the following questions and choices, pick the choice that corresponds to the best question.\n\n" + 
+                SHOTS[4] + '\n' +
+                f"\"{row['question']}\", \"{row['A']}, {row['B']}, {row['C']}, {row['D']}, {row['E']}\" -> \""), self._token_set),
+
+            'open_ai_2shot': (lambda row: (
+                f"Given the following questions and choices, pick the choice that corresponds to the best question.\n\n" + 
+                SHOTS[4] + '\n' +
+                SHOTS[5] + '\n' +
+                f"\"{row['question']}\", \"{row['A']}, {row['B']}, {row['C']}, {row['D']}, {row['E']}\" -> \""), self._token_set),
+
         }
 
         return templates
