269f3b084599458363430b24621af4b877fee1d9
Josh Robinson <Robinson.josh.j@gmail.com>
2021-11-01 15:05:00 -0600

    Add code for fixing token sets (for reference)

--- mutualinf/token_set_patch.py
+++ mutualinf/token_set_patch.py
@@ -8,6 +8,7 @@ from boolq import BoolqDataset
 from common_sense_qa import CommonSenseQaDataset
 
 import glob
+import numpy as np
 import os
 import pandas as pd
 
@@ -24,6 +25,21 @@ CLS_DIR = {
 }
 
 
+def get_new_token_set(row, obj):
+
+    try:
+        ts = obj._get_templates()[row.template_name][1]
+    except KeyError:
+        # This template_name no longer exists
+        print("Template name", row.template_name, "does not exist")
+        return "DROP_ROW"
+
+    if callable(ts):
+        return ts(row)
+    else:
+        return ts
+
+
 for cls, dir in CLS_DIR.items():
 
     # Save dataset pickle again with updated templates
@@ -33,16 +49,23 @@ for cls, dir in CLS_DIR.items():
     for f in glob.glob(f"{dir}/*_processed.pkl"):
         os.remove(f)
 
-    # Fix experiment file key sets
+    # Fix experiment file token sets
     for fname in glob.glob(f"{dir}/exp_results_*"):
+
+        print("Updating token sets for", fname)
 
         # Load pickle file associated with fname
         df = pd.read_pickle(fname)
 
-        # Iterate over rows and replace token sets
-        for _, row in df.iterrows():
-            template_name = row["template_name"]
-            row["token_sets"] = obj._get_templates()[template_name][1]
+        # Update token sets
+        df["token_sets"] = df.apply(lambda x: get_new_token_set(x, obj),
+                                    axis=1)
+
+        # Drop all rows from df where token_sets has a value of "DROP_ROW"
+        df = df[df.token_sets != "DROP_ROW"]
+
+        # Assert we have 10,000 rows in df (500 samples * 20 templates)
+        assert len(df) == 10000, f"Got {len(df)} rows instead of 10000"
 
         # Pickle df
         df.to_pickle(fname)
