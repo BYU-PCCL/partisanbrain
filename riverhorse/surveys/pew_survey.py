eb0876c9f7ec931893e7ccc0ba868509940904cf
Josh Robinson <Robinson.josh.j@gmail.com>
2022-01-05 14:43:43 -0700

    Add starting point for surveys

---
+++ riverhorse/surveys/pew_survey.py
@@ -0,0 +1,23 @@
+from parent_dir import Survey, UserInterventionNeededError
+
+
+class PewSurvey(Survey):
+
+    def __init__(self):
+        super().__init__()
+
+    def download_data(self):
+        pass
+
+    def modify_data(self, df):
+        pass
+
+    def get_dv_questions(self):
+        pass
+
+
+if __name__ == "__main__":
+    # Make sure this runs without errors after pulling the most
+    # recent code from GitHub. See surveys/example.py for more
+    # information on making your subclass.
+    s = PewSurvey()
