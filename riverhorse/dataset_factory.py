da3064f7c0aea6b673e423d44c5520d17115a8e3
Josh Robinson <Robinson.josh.j@gmail.com>
2022-01-06 13:01:11 -0700

    Make it so dataset_factory can access mutualinf code

---
+++ riverhorse/dataset_factory.py
@@ -0,0 +1,11 @@
+from mi_codebase import Dataset
+
+
+class DatasetFactory:
+
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ riverhorse/mi_codebase.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+# Get name of directory where this file is
+current = os.path.dirname(os.path.realpath(__file__))
+
+# Get parent directory name from the current directory
+parent = os.path.dirname(current)
+
+# Add the mutualinf to sys.path
+sys.path.append(parent + "/mutualinf")
+
+# Import mutual information modules
+from dataset import Dataset
