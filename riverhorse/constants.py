12e6a246cc64e240f28c3abdad6cee1f3372f7b5
Josh Robinson <Robinson.josh.j@gmail.com>
2022-01-06 13:00:29 -0700

    Factor out constants

---
+++ riverhorse/constants.py
@@ -0,0 +1,5 @@
+DEMOGRAPHIC_COLNAMES = ["age", "gender", "party",
+                        "ideology", "education", "income",
+                        "religion", "race_ethnicity",
+                        "region", "marital_status"]
+N_DVS = 20
