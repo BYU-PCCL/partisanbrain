7c06c5b049af829a53516bb61449c09b3d0c31cd
Josh Robinson <Robinson.josh.j@gmail.com>
2022-01-07 14:32:08 -0700

    Fix n=n thing

--- riverhorse/survey_factories/prri_survey_factory.py
+++ riverhorse/survey_factories/prri_survey_factory.py
@@ -6,7 +6,7 @@ class PrriFactory(DatasetFactory):
     def __init__(self, survey_obj, sample_seed=0, n=None):
         super().__init__(survey_obj=survey_obj,
                          sample_seed=sample_seed,
-                         n=None)
+                         n=n)
 
     def modify_data(self, df):
         pass
