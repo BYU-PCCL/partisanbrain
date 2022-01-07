6f32867504312ef4ef7df3b7bab7ca1cb4973537
chris-luangrath <thechrisluangrath@gmail.com>
2022-01-07 16:17:01 -0700

    banking on stuff

--- riverhorse/survey_factories/cces_survey_factory.py
+++ riverhorse/survey_factories/cces_survey_factory.py
@@ -1,4 +1,6 @@
 from parent_dir import DatasetFactory
+import pandas as pd
+import numpy as np
 
 
 class CcesFactory(DatasetFactory):
@@ -9,11 +11,93 @@ class CcesFactory(DatasetFactory):
                          n=None)
 
     def modify_data(self, df):
+        print(df)
+        print(df.unique())
         pass
 
     def get_templates(self):
         pass
 
+    def make_backstory1(self, row):
+        '''
+        list style backstory dropping nans
+        '''
+        dictionary = {
+            'age': {
+                '-1': '',
+                'nan': '',
+                'default': f'''I am {row['age']} years old. ''',
+            },
+            'gender': {
+                np.nan: '',
+                'nan': '',
+                'default': f'''I am {row['gender']}. ''',
+            },
+            'party': {
+                np.nan: '',
+                'nan': '',
+                'None/Independent': 'I am an independent. ',
+                'default': f'''I am a {row['party']}. ''',
+            },
+            'ideology': {
+                np.nan: '',
+                'nan': '',
+                'default': f'''Ideologically, I am {str(row['ideo']).strip().lower()}. ''',
+            },
+            'education': {
+                np.nan: '',
+                'nan': '',
+                'trade school': 'I attended trade school. ',
+                'professional school degree': 'I have a professional school degree. ',
+                "Bachelor's degree": "I have a Bachelor's degree. ",
+                'less than high school': "I didn't finish high school. ",
+                'high school graduate': "I graduated high school. ",
+                "Master's degree": "I have a Master's degree. ",
+                'associate degree': "I have an associate's degree. ",
+                'some college but no degree': "I attended some college. ",
+                # no default, should be exhaustive
+            },
+            #income
+            # i make between blank and blank per year
+            'religion': {
+                np.nan: '',
+                'nan': '',
+                'default': f'''I am {row['gender']}. ''',
+                'Undifferentiated Protestant': 'Religiously, I am protestant. ',
+                'Undifferentiated Protstant': 'Religiously, I am protestant. ',
+                'Other Christian': 'Religiously, I am Christian. ',
+                'default': f'Religiously, I identify as {row["religion"]}. ',
+            },
+            'race': {
+                np.nan: '',
+                'nan': '',
+                'default': f'''I am {row['race']}. ''',
+
+                'default': f'Racially, I identify as {row["race"]}. ',
+            },
+            #region
+            'marital': {
+                np.nan: '',
+                'nan': '',
+                'never married': 'I have never married. ',
+                'default': f'''I am {row['marital']}. ''',
+            },
+            
+            
+            
+            
+        }
+        backstory = ''
+        for key in dictionary.keys():
+            val = row[key]
+            if val in dictionary[key]:
+                backstory += dictionary[key][val]
+            else:
+                backstory += dictionary[key]['default']
+        return backstory
+
 
 if __name__ == "__main__":
-    factory = CcesFactory()
+    factory = CcesFactory(CcesSurvey())
+    # alex needs to finish CcesSurvey Class
+    # josh needs to finish import thing
