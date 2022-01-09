87ac2f424aebece31a723cd5e85fde8d69b7907b
Sophie Gee <sophieggee@icloud.com>
2022-01-08 19:08:11 -0700

    pushing prri_survey_factory.py for proof read

--- riverhorse/survey_factories/prri_survey_factory.py
+++ riverhorse/survey_factories/prri_survey_factory.py
@@ -12,101 +12,6 @@ class PrriFactory(DatasetFactory):
     def modify_data(self, df):
         pass
 
-    def get_templates(self):
-        return {
-        "voting_frequency" : {
-            "finish_sentence" : (lambda row: (f"{make_backstory1(row)}\n\n"
-                f"When asked how often I would say that I vote between the options of always, nearly always, in about half of elections, seldom, or never,\n\n"
-                f"I would say that I vote"), {}),
-            "delimiter_qa" : (lambda row: (f"{make_backstory1(row)}\n\n"
-                f"Q: How often would you say you vote? Always, nearly always, in about half of elections, seldom, or never?\n\n"
-                f"A: I would say that I vote"), {}),
-            "0_shot_task_context_question" : (lambda row: (f"TASK: Consider the below demographic information and answer the following question.\n\n"
-                f"CONTEXT: {make_backstory2(row)}\n\n"
-                f"QUESTION: Would you say that you vote always, nearly always, in about half of elections, seldom, or never?\n\n"
-                f"ANSWER:"), {}),
-            "1_shot_task_context_question" : (lambda row: (f"TASK: Consider the below demographic information and answer the following question.\n\n"
-                f"CONTEXT: {make_backstory2_shot(row)}\n\n"
-                f"QUESTION: How often would you say that you vote?\n\n"
-                f"ANSWER: Always\n\n"
-                f"TASK: Consider the below demographic information and answer the following question.\n\n"
-                f"CONTEXT: {make_backstory2(row)}\n\n"
-                f"QUESTION: Would you say that you vote always, nearly always, in about half of elections, seldom, or never?\n\n"
-                f"ANSWER:"), {}),
-            "explicit_instructions" : (lambda row: (f"The PRRI is a nationally representative survey of voters in American Elections. Below are examples of respondents answering various questions. Please complete what you would guess the right answers to those questions to be.\n\n"
-                f"{make_backstory3(row)}\n\n"
-                f"Q: How often would you say that you vote?\n"
-                f"A:"), {}),
-            "implicit_instructions" : (lambda row: (f"P1: {make_backstory1(row)}\n"
-                f"P2: When asked how often you vote, between the options of always, nearly always, in about half of elections, seldom, or never,\n"
-                f"P1: I would say that I vote"), {}),
-                "enumerated_response" : (lambda row: (f"{make_backstory1(row)} Between increased, decreased, or stay the same, federal spending on protecting the environment should be"), {}),
-                
-                "non_enumerated_response" : (lambda row: (f"{make_backstory1(row)} Federal spending on protecting the environment should be"), {}),
-
-                "0_shot_first_person_backstory" : (lambda row: (f"{make_backstory1(row)} I think federal spending on protecting the environment should be"), {}),
-
-                "1_shot_first_person_backstory" : (lambda row: (f"{make_backstory1_shot()} I think environmental protection is important.\n\n"
-                                                                f"{make_backstory1(row)} I think federal spending on protecting the environment should be"), {}),
-
-                "0_shot_chapter_quiz" : (lambda row: (f"CHAPTER QUIZ\n\nBACKSTORY:\n{make_backstory1(row)}\n\nQUESTIONS:\n"
-                                                f"1) According to the above backstory, would this person likely support green reforms?\n"
-                                                f"2) According to the above backstory, would this person want the federal spending protecting the environment to be increased, decreased, or kept the same?\n\n"
-                                                f"ANSWERS:\n1) Yes\n2)"), {}),
-                
-                "1_shot_chapter_quiz" : (lambda row: (f"CHAPTER QUIZ\n\nBACKSTORY:\n{make_backstory1(row)}\n\nQUESTIONS:\n"
-                                                f"1) According to the above backstory, would this person likely support green reforms?\n"
-                                                f"2) According to the above backstory, would this person want the federal spending protecting the environment to be increased, decreased, or kept the same?\n\n"
-                                                f"ANSWERS:\n1) Yes\n2)"), {}),
-
-                "0_shot_survey_response" : (lambda row: (f"{make_backstory4(row)}\n\nQuestion 11: Should federal spending on protecting the environment be increased, decreased, or kept the same?\nAnswer 11:"), {}),
-
-                "1_shot_survey_response" : (lambda row: (f"{make_backstory4(row)}\n\nQuestion 11: Do you consider environment protection to be important to you?\n"
-                                                        f"Answer 11: Yes\n\nQuestion 12: Should federal spending on protecting the environment be increased, decreased, or kept the same?\nAnswer 12:"), {}),
-
-                "0_shot_heavy_delimited" : (lambda row: (f"Given the backstory and question, please answer the question appropriately.\n\n"
-                                                f"\'\'\'Backstory -- {make_backstory1(row)}\'\'\', \'\'\'Should federal spending on protecting the environment be increased, decreased, or kept the same?\'\'\n\n"
-                                                f" -> \'\'\'"), {}),
-
-                "1_shot_heavy_delimited" : (lambda row: (f"Given the backstory and question, please answer the question appropriately.\n\n"
-                                                f"\'\'\'Backstory -- {make_backstory1_shot()}\'\'\', \'\'\'Do you consider environment protection to be important to you?\'\'\n\n"
-                                                f" -> \'\'\'Yes\'\'\'\n\n"
-                                                f"\'\'\'Backstory -- {make_backstory1(row)}\'\'\', \'\'\'Should federal spending on protecting the environment be increased, decreased, or kept the same?\'\'\n\n"
-                                                f" -> \'\'\'"), {}),
-
-                "0_shot_multiple_choice" : (lambda row: (f"Background info: {make_backstory2(row)}\n\n"
-                                                        f"Question 1: Should federal spending on protecting the environment be increased, decreased, or kept the same?\n"
-                                                        f"A: Increased\n"
-                                                        f"B: Decreased\n"
-                                                        f"C: Kept the same\n"
-                                                        f"Answer 1:"), {'Increased' : ['A', 'Increased'], 'Decreased' : ['B', 'Decreased'], 'Kept the same' : ['C', 'Kept']}),
-                
-                "1_shot_multiple_choice" : (lambda row: (f"Background info: {make_backstory2(row)}\n\n"
-                                                        f"Question 1: Do you consider environment protection to be important to you?\n"
-                                                        f"A: Yes\n"
-                                                        f"B: No\n"
-                                                        f"C: Indifferent\n"
-                                                        f"Answer 1: Yes\n\n"
-                                                        f"Question 2: Should federal spending on protecting the environment be increased, decreased, or kept the same?\n"
-                                                        f"A: Increased\n"
-                                                        f"B: Decreased\n"
-                                                        f"C: Kept the same\n"
-                                                        f"Answer 2:"), {}),
-                
-                "0_shot_direct_mapping" : (lambda row: (f"{make_backstory1(row)}\n\n"
-                                                        f"Question 1: Should federal spending on protecting the environment be increased, decreased, or kept the same?\n"
-                                                        f"Answer 1 (Increased, Decreased, Kept the same):"), {}),
-                
-                "1_shot_direct_mapping" : (lambda row: (f"{make_backstory1_shot()}\n\n"
-                                                        f"Question 1: Do you consider environment protection to be important to you?\n"
-                                                        f"Answer 1: Yes"
-                                                        f"{make_backstory1(row)}\n\n"                                                      
-                                                        f"Question 2: Should federal spending on protecting the environment be increased, decreased, or kept the same?\n"
-                                                        f"Answer 2 (Increased, Decreased, Kept the same):"), {}),
-
-            }
-        }
-
     def make_backstory1(self):
         party_lambda = lambda row: ("don\'t identify as a member of a major party" if "None" in (row['party']) else "identify as a member of the " + row['party'])
         # create a generic backstory using values in the demographics
@@ -151,6 +56,148 @@ class PrriFactory(DatasetFactory):
                             f"Question 8: What is your race/ethnicity?\nAnswer 8: {row['race_ethnicity']}\n\nQuestion 9: What region of the country are your from?\nAnswer 9: {row['region']}"
                             f"Question 10: What is your marital status?\nAnswer 10: {row['marital_status']}")
 
+    def get_templates(self):
+        return {
+        "voting_frequency" : {
+            "finish_sentence" : (lambda row: (f"{make_backstory1(row)}\n\n"
+                f"When asked how often I would say that I vote between the "
+                f"options of always, nearly always, in about half of elections, seldom, or never,\n\n"
+                f"I would say that I vote"), {}),
+
+            "delimiter_qa" : (lambda row: (f"{make_backstory1(row)}\n\n"
+                f"Q: How often would you say you vote? Always, nearly always, "
+                f"in about half of elections, seldom, or never?\n\n"
+                f"A: I would say that I vote"), {}),
+
+            "0_shot_task_context_question" : (lambda row: (f"TASK: Consider the below demographic "
+                f"information and answer the following question.\n\n"
+                f"CONTEXT: {make_backstory2(row)}\n\n"
+                f"QUESTION: Would you say that you vote always, nearly always, in about half of elections, "
+                f"seldom, or never?\n\n"
+                f"ANSWER:"), {}),
+
+            "1_shot_task_context_question" : (lambda row: (f"TASK: Consider the below demographic "
+                "information and answer the following question.\n\n"
+                f"CONTEXT: {make_backstory2_shot(row)}\n\n"
+                f"QUESTION: How often would you say that you vote?\n\n"
+                f"ANSWER: Always\n\n"
+                f"TASK: Consider the below demographic information and answer the following question.\n\n"
+                f"CONTEXT: {make_backstory2(row)}\n\n"
+                f"QUESTION: Would you say that you vote always, nearly always, in about half of elections, "
+                f"seldom, or never?\n\n"
+                f"ANSWER:"), {}),
+
+            "explicit_instructions" : (lambda row: (f"The PRRI is a nationally representative survey of voters"
+                f" in American Elections. Below are examples of respondents answering various questions. "
+                f"Please complete what you would guess the right answers to those questions to be.\n\n"
+                f"{make_backstory3(row)}\n\n"
+                f"Q: How often would you say that you vote?\n"
+                f"A:"), {}),
+
+            "implicit_instructions" : (lambda row: (f"P1: {make_backstory1(row)}\n"
+                f"P2: When asked how often you vote, between the options of always, "
+                f"nearly always, in about half of elections, seldom, or never,\n"
+                f"P1: I would say that I vote"), {}),
+
+            "enumerated_response" : (lambda row: (f"{make_backstory1(row)} Between voting always," 
+                f"nearly always, in about half of elections, seldom, or never, I would say that I vote"), {}),
+                
+            "non_enumerated_response" : (lambda row: (f"{make_backstory1(row)} I would say that I vote"), {}),
+
+            "0_shot_first_person_backstory" : (lambda row: (f"{make_backstory1(row)} When asked how often I vote, "
+                f"I would say that I vote"), {}),
+
+            "1_shot_first_person_backstory" : (lambda row: (f"{make_backstory1_shot()} I would say that I vote always.\n\n"
+                f"{make_backstory1(row)} Between the options of always, almost always, in about half of elections,"
+                f" seldom, or never, "
+                f"I would say that I vote"), {}),
+
+            "0_shot_chapter_quiz" : (lambda row: (f"CHAPTER QUIZ\n\nBACKSTORY:\n{make_backstory1(row)}\n\nQUESTION:\n"
+                f"According to the above backstory, how often would this person be likely to vote?\n"
+                f"A) Always\n"
+                f"B) Almost always\n"
+                f"C) In about half of elections\n"
+                f"D) Seldom\n"
+                f"ANSWER:"), {}),
+                
+            "1_shot_chapter_quiz" : (lambda row: (f"CHAPTER QUIZ\n\nBACKSTORY:\n{make_backstory1_shot(row)}\n\nQUESTION:\n"
+                f"According to the above backstory, how often would this person be likely to vote?\n"
+                f"A) Always\n"
+                f"B) Almost always\n"
+                f"C) In about half of elections\n"
+                f"D) Seldom\n"
+                f"ANSWER:\n"
+                f"A"
+                f"\n\nBACKSTORY:\n{make_backstory1_shot(row)}\n\nQUESTION:\n"
+                f"According to the above backstory, how often would this person be likely to vote?\n"
+                f"A) Always\n"
+                f"B) Almost always\n"
+                f"C) In about half of elections\n"
+                f"D) Seldom\n"
+                f"ANSWER:"), {'Always' : ['A', 'Always'], 'Almost always' : ['B', 'Almost'], 
+                'In about half of elections' : ['C', 'In'], 'Seldom':['D','Seldom']}),
+
+            "0_shot_survey_response" : (lambda row: (f"{make_backstory4(row)}\n\nQuestion 11: Do you vote always, "
+                f"almost always, in about half of elections, seldom, or never?\nAnswer 11:"), {}),
+
+            "1_shot_survey_response" : (lambda row: (f"{make_backstory4(row)}\n\nQuestion 11: Do you have the "
+                f"ability to vote?\n"
+                f"Answer 11: Yes\n\nQuestion 12: Do you vote always, almost always, in about half of elections, "
+                f"seldom, or never?\nAnswer 12:"), {}),
+
+            "0_shot_heavy_delimited" : (lambda row: (f"Given the backstory and question, "
+                f"please answer the question appropriately.\n\n"
+                f"\'\'\'Backstory -- {make_backstory1(row)}\'\'\', \'\'\'Would you say that you vote always, "
+                f"almost always, in about half of elections, seldom, or never?\'\'\n\n"
+                f" -> \'\'\'"), {}),
+
+            "1_shot_heavy_delimited" : (lambda row: (f"Given the backstory and question, "
+                f"please answer the question appropriately.\n\n"
+                f"\'\'\'Backstory -- {make_backstory1_shot()}\'\'\', \'\'\'Do you consider voting to be "
+                f"important to you?\'\'\n\n"
+                f" -> \'\'\'Yes\'\'\'\n\n"
+                f"\'\'\'Backstory -- {make_backstory1(row)}\'\'\', \'\'\'Do you vote always, almost always,"
+                f" in about half of elections, seldom, or never?\'\'\n\n"
+                f" -> \'\'\'"), {}),
+
+            "0_shot_multiple_choice" : (lambda row: (f"Background info: {make_backstory2(row)}\n\n"
+                f"Question 1: Do you vote always, almost always, in about half of elections, seldom, or never?\n"
+                f"A: Always\n"
+                f"B: Almost Always\n"
+                f"C) In about half of elections\n"
+                f"D) Seldom\n"
+                f"ANSWER:"), {'Always' : ['A', 'Always'], 'Almost always' : ['B', 'Almost'], 
+                'In about half of elections' : ['C', 'In'], 'Seldom':['D','Seldom']}),
+                
+            "1_shot_multiple_choice" : (lambda row: (f"Background info: {make_backstory1_shot(row)}\n\n"
+                f"Question 1: How often do you vote?\n"
+                f"A: Always\n"
+                f"B: Almost Always\n"
+                f"C) In about half of elections\n"
+                f"D) Seldom\n"
+                f"Answer: \nA\n"
+                f"Background info: {make_backstory1(row)}\n\n"
+                f"Question 1: How often do you vote?\n"
+                f"A: Always\n"
+                f"B: Almost Always\n"
+                f"C) In about half of elections\n"
+                f"D) Seldom\n"
+                f"Answer:"), {'Always' : ['A', 'Always'], 'Almost always' : ['B', 'Almost'], 
+                'In about half of elections' : ['C', 'In'], 'Seldom':['D','Seldom']}),
+                
+            "0_shot_direct_mapping" : (lambda row: (f"{make_backstory1(row)}\n\n"
+                f"Question 1: Do you vote always, almost always, in about half of elections, seldom, or never?\n"
+                f"Answer 1 (always, almost always, in about half of elections, seldom, never):"), {}),
+                
+            "1_shot_direct_mapping" : (lambda row: (f"{make_backstory1_shot()}\n\n"
+                f"Question 1: Do you consider voting important to you?\n"
+                f"Answer 1: Yes"
+                f"{make_backstory1(row)}\n\n"                                                      
+                f"Question 2: Do you vote always, almost always, in about half of elections, seldom, or never?\n"
+                f"Answer 2 (always, almost always, in about half of elections, seldom, never):"), {}),
+
+            }
+        }
 
 if __name__ == "__main__":
     factory = PrriFactory(PrriSurvey())
