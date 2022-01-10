307f8385bf907ff46a26cca3dedd41d4e6a41bd8
Alex Shaw <alexgshaw64@gmail.com>
2022-01-10 11:24:33 -0700

    fixed constants

--- riverhorse/constants.py
+++ riverhorse/constants.py
@@ -1,8 +1,16 @@
-import pathlib as Path
+from pathlib import Path
 
-DEMOGRAPHIC_COLNAMES = ["age", "gender", "party",
-                        "ideology", "education", "income",
-                        "religion", "race_ethnicity",
-                        "region", "marital_status"]
+DEMOGRAPHIC_COLNAMES = [
+    "age",
+    "gender",
+    "party",
+    "ideology",
+    "education",
+    "income",
+    "religion",
+    "race_ethnicity",
+    "region",
+    "marital_status",
+]
 N_DVS = 20
 SURVEY_DATA_PATH = Path(__file__).parent.absolute() / "survey_data"
