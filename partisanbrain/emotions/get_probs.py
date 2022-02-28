c169a26d8f7ada5f73a790a56f89def19c83b186
Alex Shaw <alexgshaw64@gmail.com>
2022-02-28 15:19:32 -0700

    reverse edits

--- partisanbrain/emotions/get_probs.py
+++ partisanbrain/emotions/get_probs.py
@@ -53,7 +53,7 @@ def get_probs(data_filename, mask_filename):
 
     for i, row in tqdm(df.iterrows()):
         input = tokenizer.encode(row.sentence.strip(), return_tensors="pt")
-        input = input.to(DEVICE)
+        input.to(DEVICE)
 
         with torch.no_grad():
             output = model(**input)
