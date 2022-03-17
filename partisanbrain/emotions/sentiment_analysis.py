f212521cb1dfbbe6d99e1c76144d5471afc44724
Lexi <lexi@deloreyfamily.org>
2022-03-17 12:26:23 -0600

    applying alex's sentiment analysis fix to this branch

--- partisanbrain/emotions/sentiment_analysis.py
+++ partisanbrain/emotions/sentiment_analysis.py
@@ -1,20 +1,24 @@
 from transformers import pipeline
 import pandas as pd
+import torch
+
+
+DEVICE = 0 if torch.cuda.is_available() else None
 
 
 class SentimentClassifier:
     def __init__(self, input_filename):
         self.df = pd.read_csv(input_filename)
         self.mod_df = self.df.copy()
-        self.classifier = pipeline("sentiment-analysis")
+        self.classifier = pipeline("sentiment-analysis", device=DEVICE)
 
     def classify_sentiment(self):
         results = self.classifier(self.df.sentence.to_list())
-        scores = [result[0]["score"] for result in results]
-        sentiments = [result[0]["label"] for result in results]
+        scores = [result["score"] for result in results]
+        sentiments = [result["label"] for result in results]
 
         self.mod_df["score"] = scores
-        self.mod_df["semtiment"] = sentiments
+        self.mod_df["sentiment"] = sentiments
 
     def get_value_counts(self):
         altered_mask = self.mod_df.label == 1
