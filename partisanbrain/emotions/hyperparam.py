df435f876460fe3a621da44ed77978179f42a713
Alex Shaw <alexgshaw64@gmail.com>
2022-03-30 16:18:33 -0600

    hyperparams

--- partisanbrain/emotions/hyperparam.py
+++ partisanbrain/emotions/hyperparam.py
@@ -15,6 +15,23 @@ torch.seed(0)
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 FORCE_EMOTION = "negative"
 
+# hyperparams = {
+#     "selection_method": [
+#         "correlation",
+#         "logistic_regression",
+#         "pca_correlation",
+#         "pca_log_reg",
+#     ],
+#     "n_neurons": [100, 200, 500, 1000, 5000],
+#     "percentile": [0.5, 0.8, 1],
+# }
+# lda_hyperparams = {
+#     "selection_method": ["lda"],
+#     "layer_selection_method": ["correlation", "logistic_regression"],
+#     "n_layers": [1, 10, 25, 49],
+#     "percentile": [0.5, 0.8, 1],
+# }
+
 hyperparams = {
     "selection_method": [
         "correlation",
@@ -22,15 +39,16 @@ hyperparams = {
         "pca_correlation",
         "pca_log_reg",
     ],
-    "n_neurons": [100, 200, 500, 1000, 5000],
-    "percentile": [0.5, 0.8, 1],
+    "n_neurons": [100],
+    "percentile": [1],
 }
 lda_hyperparams = {
     "selection_method": ["lda"],
     "layer_selection_method": ["correlation", "logistic_regression"],
-    "n_layers": [1, 10, 25, 49],
-    "percentile": [0.5, 0.8, 1],
+    "n_layers": [49],
+    "percentile": [1],
 }
+
 selection_method_to_force_with = {
     "correlation": "per_neuron",
     "logistic_regression": "per_neuron",
@@ -52,7 +70,8 @@ X = output["activations"]
 y = output["targets"].squeeze()
 
 perplexity_df = pd.read_csv("data/wiki.csv")
-perplexity_analyzer = PerplexityAnalyzer(model, tokenizer, df=perplexity_df)
+# perplexity_analyzer = PerplexityAnalyzer(model, tokenizer, df=perplexity_df)
+perplexity_analyzer = PerplexityAnalyzer(model, tokenizer, df=perplexity_df.sample(10))
 
 results = []
 neuron_selector = NeuronSelector(input_filename="output/output.npz", device=DEVICE)
@@ -74,7 +93,8 @@ for params in ParameterGrid(hyperparams):
     )
     df = generator.generate_samples(
         prompt=prompt,
-        n_sequences=1000,
+        n_sequences=10,
+        # n_sequences=1000,
     )
 
     # Classify the sentiment of the generated sentences
@@ -122,7 +142,8 @@ for params in ParameterGrid(lda_hyperparams):
     )
     df = generator.generate_samples(
         prompt=prompt,
-        n_sequences=1000,
+        # n_sequences=1000,
+        n_sequences=10,
     )
 
     # Classify the sentiment of the generated sentences
